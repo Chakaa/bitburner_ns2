793bd65f5436e739f962debf17c6c334165982ac
Chakaa <pberteigne@gmail.com>
2022-01-01 21:24:59 +0100

    deploy

---
+++ chakaa.bin.hack.js
@@ -0,0 +1,12 @@
+/**
+ * A *very basic* script for growing a host.
+ */
+/** @param {NS} ns **/
+
+export async function main(ns) {
+    const host = ns.args[0] || ns.getHostname();
+
+    while (true) {
+        await ns.hack(host);
+    }
+}
---
+++ chakaa.init.js
@@ -0,0 +1,29 @@
+const SCRIPTS = [
+    'chakaa.bin.grow.js',
+    'chakaa.bin.hack.js',
+    'chakaa.bin.weaken.js',
+  ];
+
+const joinPath = (...parts) => parts.join('/');
+
+export async function main(ns) {
+  const scriptServer = ns.args[0] || 'http://localhost:8080';
+  const scriptRoot = 'scripts';
+
+  for (const script of SCRIPTS) {
+    const url = joinPath(scriptServer, scriptRoot, script);
+
+    ns.tprint(`Downloading ${script}`);
+    const success = await ns.wget(url, script);
+
+    if (!success) {
+      ns.tprint(`Unable to download script from url: ${url}`);
+    }
+
+    // Wait a little while to give the RAM calculations some time
+    // to finish.
+    // await ns.sleep(1000);
+  }
+
+  ns.tprint('Botnet deployed');
+}
